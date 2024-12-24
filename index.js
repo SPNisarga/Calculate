2a0c8b9d487901e67d659fdb6550c2b4710460d2
SPNisarga <nisargasp090802@gmail.com>
2024-12-23 19:21:01 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+function clearDisplay()
+        {
+            document.getElementById("display").value = '';
+        }
+
+        function appendToDisplay(value)
+        {
+            document.getElementById("display").value += value;
+ 
+        }
+
+        function calulate()
+        {
+            try
+            {
+                const result = eval(document.getElementById("display").value)
+                document.getElementById("display").value = result;
+            }
+            catch(error)
+            {
+                document.getElementById("display").value = 'error';
+
+            }
+        }
